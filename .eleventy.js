0a1fa93ca6cc518afb29259b156515e458a2cef3
Diego Auyón <diegoeauyonh@gmail.com>
2021-02-03 00:24:35 -0600

    WIP cssclean

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const CleanCSS = require("clean-css");
+
 module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "./assets": "" });
+
+  // Filtros
+
+  // Optimiza css https://github.com/jakubpawlowicz/clean-css
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
 
   return {
     passthroughFileCopy: true,
